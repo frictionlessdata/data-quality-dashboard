e2d066bd501af1393f4e46f66ff2cd7e84d934e5
Paul Walsh <paulywalsh@gmail.com>
2015-02-09 10:00:22 +0200

    Dump in test data for demonstration, until API calls are working.

--- dashboard/scripts/utils/APIUtils.js
+++ dashboard/scripts/utils/APIUtils.js
@@ -9,19 +9,475 @@ var instance = backend + '/instance.json';
 
 // temp!!
 var _publishers = [
-    {id: 'gb_this', name: 'Publisher1', url: 'http://url', something: 'something here2', score: 7, source_count: 21, parent: null},
-    {id: 'gb_that', name: 'Publisher2', url: 'http://url', something: 'something here2', score: 4, source_count: 13, parent: null},
-    {id: 'gb_other', name: 'Publisher21', url: 'http://url', something: 'something here21', score: 4, source_count: 13, parent: 'gb_that'}
+  {
+    "id":"xx_dept1",
+    "parent_id":"",
+    "name":"Department 1",
+    "description":"",
+    "url":"http://www.example.com/dept1",
+    "jurisdiction_code":"XX",
+    "email":"dept1-admin@example.com",
+    "address":"",
+    "contact":"Dept1 Admin",
+    "score":8,
+    "source_count":2
+  },
+  {
+    "id":"xx_dept2",
+    "parent_id":"",
+    "name":"Department 2",
+    "description":"",
+    "url":"http://www.example.com/dept2",
+    "jurisdiction_code":"XX",
+    "email":"dept2-admin@example.com",
+    "address":"",
+    "contact":"Dept2 Admin",
+    "score":8,
+    "source_count":2
+  },
+  {
+    "id":"xx_dept3",
+    "parent_id":"",
+    "name":"Department 3",
+    "description":"",
+    "url":"http://www.example.com/dept3",
+    "jurisdiction_code":"XX",
+    "email":"dept3-admin@example.com",
+    "address":"",
+    "contact":"Dept3 Admin",
+    "score":8,
+    "source_count":2
+  },
+  {
+    "id":"xx_dept4",
+    "parent_id":"",
+    "name":"Department 4",
+    "description":"",
+    "url":"http://www.example.com/dept4",
+    "jurisdiction_code":"XX",
+    "email":"dept4-admin@example.com",
+    "address":"",
+    "contact":"Dept4 Admin",
+    "score":8,
+    "source_count":2
+  },
+  {
+    "id":"xx_dept5",
+    "parent_id":"",
+    "name":"Department 5",
+    "description":"",
+    "url":"http://www.example.com/dept5",
+    "jurisdiction_code":"XX",
+    "email":"dept5-admin@example.com",
+    "address":"",
+    "contact":"Dept5 Admin",
+    "score":8,
+    "source_count":2
+  },
+  {
+    "id":"xx_dept6",
+    "parent_id":"",
+    "name":"Department 6",
+    "description":"",
+    "url":"http://www.example.com/dept6",
+    "jurisdiction_code":"XX",
+    "email":"dept6-admin@example.com",
+    "address":"",
+    "contact":"Dept6 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept7",
+    "parent_id":"",
+    "name":"Department 7",
+    "description":"",
+    "url":"http://www.example.com/dept7",
+    "jurisdiction_code":"XX",
+    "email":"dept7-admin@example.com",
+    "address":"",
+    "contact":"Dept7 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept8",
+    "parent_id":"",
+    "name":"Department 8",
+    "description":"",
+    "url":"http://www.example.com/dept8",
+    "jurisdiction_code":"XX",
+    "email":"dept8-admin@example.com",
+    "address":"",
+    "contact":"Dept8 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept9",
+    "parent_id":"",
+    "name":"Department 9",
+    "description":"",
+    "url":"http://www.example.com/dept9",
+    "jurisdiction_code":"XX",
+    "email":"dept9-admin@example.com",
+    "address":"",
+    "contact":"Dept9 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept10",
+    "parent_id":"",
+    "name":"Department 10",
+    "description":"",
+    "url":"http://www.example.com/dept10",
+    "jurisdiction_code":"XX",
+    "email":"dept10-admin@example.com",
+    "address":"",
+    "contact":"Dept10 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept11",
+    "parent_id":"xx_dept1",
+    "name":"Department 11",
+    "description":"",
+    "url":"http://www.example.com/dept11",
+    "jurisdiction_code":"XX",
+    "email":"dept-1-admin@example.com",
+    "address":"",
+    "contact":"Dept11 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept12",
+    "parent_id":"xx_dept2",
+    "name":"Department 12",
+    "description":"",
+    "url":"http://www.example.com/dept12",
+    "jurisdiction_code":"XX",
+    "email":"dept-1-admin@example.com",
+    "address":"",
+    "contact":"Dept12 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept13",
+    "parent_id":"xx_dept3",
+    "name":"Department 13",
+    "description":"",
+    "url":"http://www.example.com/dept13",
+    "jurisdiction_code":"XX",
+    "email":"dept-1-admin@example.com",
+    "address":"",
+    "contact":"Dept13 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept14",
+    "parent_id":"xx_dept4",
+    "name":"Department 14",
+    "description":"",
+    "url":"http://www.example.com/dept14",
+    "jurisdiction_code":"XX",
+    "email":"dept-1-admin@example.com",
+    "address":"",
+    "contact":"Dept14 Admin",
+    "score":8,
+    "source_count":1
+  },
+  {
+    "id":"xx_dept15",
+    "parent_id":"xx_dept5",
+    "name":"Department 15",
+    "description":"",
+    "url":"http://www.example.com/dept15",
+    "jurisdiction_code":"XX",
+    "email":"dept-1-admin@example.com",
+    "address":"",
+    "contact":"Dept15 Admin",
+    "score":8,
+    "source_count":1
+  }
 ];
 var _sources = [
-    {id: 'gy12w', publisher_id: 'gb_this', name: 'Source1', url: 'https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv', schema: '', score: 9, revision: 2, report: 'http://url/', last_updated: '2015-01-01', period_id: '2015-01-01', extension: 'csv'},
-    {id: 'au23h', publisher_id: 'gb_other', name: 'Source2', url: 'https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv', schema: '', score: 6, revision: 2, report: 'http://url/', last_updated: '2015-01-01', period_id: '2015-01-01', extension: 'csv'},
-    {id: 're71q', publisher_id: 'gb_other', name: 'Source3', url: 'https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv', schema: '', score: 2, revision: 2, report: 'http://url/', last_updated: '2015-01-01', period_id: '2015-01-01', extension: 'csv'}
+  {
+    "id":"source1",
+    "publisher_id":"xx_dept1",
+    "name":"Source 1",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source2",
+    "publisher_id":"xx_dept2",
+    "name":"Source 2",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source3",
+    "publisher_id":"xx_dept3",
+    "name":"Source 3",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source4",
+    "publisher_id":"xx_dept4",
+    "name":"Source 4",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source5",
+    "publisher_id":"xx_dept5",
+    "name":"Source 5",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source6",
+    "publisher_id":"xx_dept6",
+    "name":"Source 6",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source7",
+    "publisher_id":"xx_dept7",
+    "name":"Source 7",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source8",
+    "publisher_id":"xx_dept8",
+    "name":"Source 8",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source9",
+    "publisher_id":"xx_dept9",
+    "name":"Source 9",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source10",
+    "publisher_id":"xx_dept10",
+    "name":"Source 10",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source11",
+    "publisher_id":"xx_dept11",
+    "name":"Source 11",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source12",
+    "publisher_id":"xx_dept12",
+    "name":"Source 12",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source13",
+    "publisher_id":"xx_dept13",
+    "name":"Source 13",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source14",
+    "publisher_id":"xx_dept14",
+    "name":"Source 14",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  },
+  {
+    "id":"source15",
+    "publisher_id":"xx_dept15",
+    "name":"Source 15",
+    "url":"https://raw.githubusercontent.com/okfn/tabular-validator/master/examples/valid.csv",
+    "score":8,
+    "revision":"1",
+    "schema":"",
+    "period_id":"2015-01-01",
+    "timestamp":"2015-01-01"
+  }
 ];
 var _results = [
-    {source_id: 're71q', publisher_id: 'gb_other', period_id: '2015-01-01', score: 2},
-    {source_id: 'au23h', publisher_id: 'gb_other', period_id: '2015-01-01', score: 6},
-    {source_id: 'gy12w', publisher_id: 'gb_this', period_id: '2015-01-01', score: 9}
+  {
+    "source_id":"source1",
+    "publisher_id":"xx_dept1",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source2",
+    "publisher_id":"xx_dept2",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source3",
+    "publisher_id":"xx_dept3",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source4",
+    "publisher_id":"xx_dept4",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source5",
+    "publisher_id":"xx_dept5",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source6",
+    "publisher_id":"xx_dept6",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source7",
+    "publisher_id":"xx_dept7",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source8",
+    "publisher_id":"xx_dept8",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source9",
+    "publisher_id":"xx_dept9",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source10",
+    "publisher_id":"xx_dept10",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source11",
+    "publisher_id":"xx_dept11",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source12",
+    "publisher_id":"xx_dept12",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source13",
+    "publisher_id":"xx_dept13",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source14",
+    "publisher_id":"xx_dept14",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  },
+  {
+    "source_id":"source15",
+    "publisher_id":"xx_dept15",
+    "period_id":"2015-01-01",
+    "score":8,
+    "timestamp":"2015-01-01"
+  }
 ];
 var _instance = {
     name: 'Spend Publishing Dashboard',
