7d065bc459fea3d6d53b8dae7b826d3087456e7d
hdurand <hdurand@riseup.net>
2015-05-12 10:49:34 -0400

    Fixes source table

--- dashboard/src/scripts/components/pages/Publisher.react.js
+++ dashboard/src/scripts/components/pages/Publisher.react.js
@@ -19,7 +19,7 @@ function getStateFromStores(getParams) {
         instance: InstanceStore.get(),
         publisher: PublisherStore.get(getParams.lookup),
         results: ResultStore.query({'publisher_id': getParams.lookup}),
-        sources: SourceStore.all({'publisher_id': getParams.lookup})
+        sources: SourceStore.query({'publisher_id': getParams.lookup})
     };
 }
 
