498add16a64851147c12e30891253351590635f5
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2015-05-21 14:15:48 +0000

    Add introductory text at top of main page

--- dashboard/src/scripts/components/MainOverview.react.js
+++ dashboard/src/scripts/components/MainOverview.react.js
@@ -6,6 +6,8 @@ var MainOverview = React.createClass({
     render: function() {
         return (
             <div className="container">
+                <h2>{this.props.dashboard}</h2>
+                <p>How are the government departments living up to their commitments to publish timely, high quality data on spending?</p>
                 <ul className="overview">
                     {UIUtils.makeOverview(this.props.results, 'main')}
                 </ul>
