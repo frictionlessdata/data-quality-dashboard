f2bd4308696cffb8bbea55b5b2a0033f0ac6696c
Paul Walsh <paulywalsh@gmail.com>
2016-06-14 23:06:46 +0300

    Fixes required for last commits: require sass and import Chart

--- app/ui/scripts/index.js
+++ app/ui/scripts/index.js
@@ -1,7 +1,11 @@
 'use strict'
 
+// for webpack to build our css
+require('../styles/app.scss')
+
 import 'babel-polyfill'
 import React from 'react'
+import Chart from 'chart.js'
 import { render } from 'react-dom'
 import App from './containers/App'
 
